c7da7fd28022413b2a3f1602c4f739741b441530
David Celis <me@davidcel.is>
2013-12-21 10:35:26 -0800

    Add specs for SocialMediaAccounts
    Signed-off-by: David Celis <me@davidcel.is>

--- app/models/social_media_account.rb
+++ app/models/social_media_account.rb
@@ -2,20 +2,21 @@ class SocialMediaAccount < ActiveRecord::Base
   belongs_to :socialable, polymorphic: true
 
   def url
-    self.send("#{website.downcase}_url")
+    self.send("#{website.parameterize('_')}_url")
   end
 
   private
-    def facebook_url
-      "http://www.facebook.com/#{handle}" if socialable_type == 'Brewery'
+    def facebook_fan_page_url
+      "http://www.facebook.com/#{handle}"
     end
+    alias :facebook_profile_url :facebook_fan_page_url
 
     def twitter_url
-      "http://twitter.com/#{handle}" if socialable_type == 'Brewery'
+      "http://twitter.com/#{handle}"
     end
 
     def foursquare_url
-      "http://foursquare.com/v/#{handle}" if socialable_type == 'Brewery'
+      "http://foursquare.com/v/#{handle}"
     end
 
     def untappd_url
@@ -28,6 +29,34 @@ class SocialMediaAccount < ActiveRecord::Base
     end
 
     def beeradvocate_url
-      "http://www.beeradvocate.com/beer/profile/#{handle}" if socialable_type == 'Brewery'
+      "http://www.beeradvocate.com/beer/profile/#{handle}"
+    end
+
+    def google_plus_url
+      "https://plus.google.com/#{handle}"
+    end
+
+    def flickr_url
+      "http://www.flickr.com/photos/#{handle}"
+    end
+
+    def youtube_url
+      "http://www.youtube.com/#{handle}"
+    end
+
+    def instagram_url
+      "http://instagram.com/#{handle}"
+    end
+
+    def yelp_url
+      "http://yelp.com/#{handle}"
+    end
+
+    def pinterest_url
+      "http://pinterest.com/#{handle}"
+    end
+
+    def feed_url
+      handle
     end
 end
---
+++ spec/models/social_media_account_spec.rb
@@ -0,0 +1,82 @@
+require 'spec_helper'
+require 'app/models/social_media_account'
+
+describe SocialMediaAccount do
+  %w[brewery beer].each do |klass|
+    context "for a #{klass}" do
+      let(:account) do
+        Factory.build(:social_media_account, socialable: Factory.build(klass))
+      end
+
+      ['Facebook Fan Page', 'Facebook Profile'].each do |type|
+        it 'can have a Facebook profile URL' do
+          account.website = type
+          expect(account.url).to eq("http://www.facebook.com/#{account.handle}")
+        end
+      end
+
+      it 'can have a Twitter URL' do
+        account.website = 'Twitter'
+        expect(account.url).to eq("http://twitter.com/#{account.handle}")
+      end
+
+      it 'can have a Foursquare URL' do
+        account.website = 'Foursquare'
+        expect(account.url).to eq("http://foursquare.com/v/#{account.handle}")
+      end
+
+      it 'can have an Untappd URL' do
+        account.website = 'Untappd'
+        type = account.socialable_type.downcase
+
+        expect(account.url).to eq("http://untappd.com/#{type}/#{account.handle}")
+      end
+
+      it 'can have a RateBeer URL' do
+        account.website = 'RateBeer'
+        type = account.socialable_type == 'Brewery' ? 'brewers' : 'beer'
+        expect(account.url).to eq("http://www.ratebeer.com/#{type}/#{account.socialable.slug}/#{account.handle}")
+      end
+
+      it 'can have a BeerAdvocate URL' do
+        account.website = 'BeerAdvocate'
+        expect(account.url).to eq("http://www.beeradvocate.com/beer/profile/#{account.handle}")
+      end
+
+      it 'can have a Google Plus URL' do
+        account.website = 'Google Plus'
+        expect(account.url).to eq("https://plus.google.com/#{account.handle}")
+      end
+
+      it 'can have a Flickr URL' do
+        account.website = 'Flickr'
+        expect(account.url).to eq("http://www.flickr.com/photos/#{account.handle}")
+      end
+
+      it 'can have a Youtube URL' do
+        account.website = 'YouTube'
+        expect(account.url).to eq("http://www.youtube.com/#{account.handle}")
+      end
+
+      it 'can have a Instagram URL' do
+        account.website = 'Instagram'
+        expect(account.url).to eq("http://instagram.com/#{account.handle}")
+      end
+
+      it 'can have a Yelp URL' do
+        account.website = 'Yelp'
+        expect(account.url).to eq("http://yelp.com/#{account.handle}")
+      end
+
+      it 'can have a Pinterest URL' do
+        account.website = 'Pinterest'
+        expect(account.url).to eq("http://pinterest.com/#{account.handle}")
+      end
+
+      it 'can have a feed URL' do
+        account.website = 'Feed'
+        expect(account.url).to eq(account.handle)
+      end
+    end
+  end
+end
