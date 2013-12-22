f5bd05f07e3b7b41c45569cd6a9b664141673c1e
David Celis <me@davidcel.is>
2013-12-21 21:24:10 -0800

    Update join records spec file
    Signed-off-by: David Celis <me@davidcel.is>

--- spec/models/shared_examples/join_records.rb
+++ spec/models/shared_examples/join_records.rb
@@ -2,7 +2,9 @@ shared_examples 'something that has join records' do
   let(:klass) { described_class.to_s.underscore.to_sym }
   let(:record) { Factory(klass) }
 
-  described_class.reflect_on_all_associations(:has_and_belongs_to_many).each do |association|
+  described_class.reflect_on_all_associations(:has_many).each do |association|
+    next unless association.options[:through]
+
     context "for the #{association.name} association" do
       let(:associated) { Factory(association.name.to_s.singularize) }
 
