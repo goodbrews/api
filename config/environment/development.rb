653387f3b301bd00dc5b8e3554ade218590ee153
David Celis <me@davidcel.is>
2014-01-21 14:35:47 -0800

    Remove instance of `config` from development environment
    Signed-off-by: David Celis <me@davidcel.is>

--- config/environment/development.rb
+++ config/environment/development.rb
@@ -2,7 +2,5 @@ Mail.defaults do
   delivery_method :file, location: Crepe.root.join('tmp', 'mail')
 end
 
-config.mail.raise_delivery_errors = false
-
 # Log database queries to STDOUT
 ActiveRecord::Base.logger = Crepe.logger
